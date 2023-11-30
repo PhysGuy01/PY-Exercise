800286e251ef4f92e95066d303c2d1f069d7f35f
PhysGuy01 <franci.specchia@gmail.com>
2023-11-30 22:28:32 +0100

    Update pi_chud.py

--- pi_chud.py
+++ pi_chud.py
@@ -4,24 +4,34 @@
 #           -- Author: @PhysGuy01 --
 #
 
-from decimal import Decimal, Context, setcontext
-#n = input("\nInsert the number of digits you'd like: ")
+from decimal import Decimal, Context, setcontext, ROUND_DOWN
+from time import time
 
-def sums(n_new):
-    s = 0
-    i = 0
-    fact_6k = 1
-    for k in range(n_new):
-        i += 1
-        fact_6k *= (6*k) * (i - 1)
-        print(fact_6k)
-        #s += (6*k)*(k - i)
-def fact(f): #fact(6k) --> 6k*(6k-6)*(6k-5)*
-    # fact(5) = 5*4*3*2*1
-    # 1 * 2 * 3 ...
-    for i in range (1, f):
-        i = i * (i + 1)
-        print(i)
-    print(i)
-fact(5)
-#sums(5)
+n = input("\nInsert the number of digits you'd like: ")
+
+while True: 
+    try: n = int(n); break
+    except: n = input("\nInsert the number of digits you'd like: ")
+
+start_time = time()
+
+# define a factorial function so we can use it later on...
+def fact(f):
+    setcontext(Context(prec=n+3, rounding=ROUND_DOWN))
+    z = 1
+    # not entirely sure how this works... or why...
+    # was just messing around with the numbers in range tbh.. but it works great so im not gonna question it...
+    for i in range (2, int(f) + 1): z *= Decimal(i)   
+    return Decimal(z)
+
+def sums(x):
+    S = 0
+    setcontext(Context(prec=n+3, rounding=ROUND_DOWN))
+    for k in range(x):
+        S += Decimal((((-1)**k) * fact(6*k) * (545140134*k + 13591409))) / Decimal(fact(3*k) * (fact(k) ** 3) * (640320) ** (3*k))
+    return S
+n_new = Decimal(n*0.5).to_integral_exact(ROUND_DOWN) if n > 20 else n
+pi = Decimal((426880 * Decimal(10005).sqrt())) / Decimal(sums(int(n_new)))
+setcontext(Context(prec=n, rounding=ROUND_DOWN))
+print(+Decimal(pi))
+print("\n--- %s seconds ---" % (time() - start_time))
